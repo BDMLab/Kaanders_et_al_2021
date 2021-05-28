cee383c61a30cc3b02a300830e1c91ce34d6753d
PradyS <p.sepulveda@ucl.ac.uk>
2021-05-28 14:23:17 -0400

    Included  GLAM models and analysis

---
+++ models/glam/__init__.py
@@ -0,0 +1,11 @@
+from . import utils
+from . import components
+from . import simulation
+
+from . import plots_pretty_GLAM_left2
+from . import models_boundFree
+
+
+from .models import make_models, GLAM
+from .fit import fit_models
+#from .plots import plot_fit
