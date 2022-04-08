8114402a635500a404e6870b074d30bdb99c9243
PradyS <p.sepulveda@ucl.ac.uk>
2022-04-08 16:14:10 +0100

    updated models

---
+++ models/glam/__init__.py
@@ -0,0 +1,15 @@
+from . import utils
+from . import components
+from . import simulation
+from . import driftSim
+from . import driftSim_UneqVar
+from . import plots_pretty
+from . import plots_pretty_GLAM
+from . import plots_pretty_GLAM_left
+from . import plots_pretty_GLAM_left2
+from . import models_boundFree
+
+
+from .models import make_models, GLAM
+from .fit import fit_models
+#from .plots import plot_fit
